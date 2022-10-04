cefea8b1495ec562ee669f03978b45d8532d8f46
gtelnoff <gaspard.teln@gmail.com>
2022-10-04 14:25:32 +0200

    main.c, minishell.h

---
+++ minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtelnoff <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 16:13:06 by gtelnoff          #+#    #+#             */
+/*   Updated: 2022/09/12 16:13:08 by gtelnoff         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_h
+# define MINISHELL_h
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_data
+{
+
+}		t_data;
+# endif
